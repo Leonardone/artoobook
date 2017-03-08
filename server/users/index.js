871a6e04ebde0b8fe352386a3d43d91d96e27939
leo <njuccon@hotmail.it>
2017-03-08 18:08:57 +0100

    aggiustato funzioni

--- server/users/index.js
+++ server/users/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 var users = require('./users.controller.js')
 
 //listautenti
-router.get('/', users.getusers);
+router.get('/', users.getuser);
+//Iscrizione listautenti
+router.post('/', users.createuser);
+//dettaglio utenti
+router.get('/:id', users.detailuser);
+//elimina utente
+router.delete('/:id', users.deleteuser);
+//aggiorna utente
+router.put('/:id', users.updateuser);
 
 
 
--- server/users/users.controller.js
+++ server/users/users.controller.js
@@ -1,8 +1,28 @@
 module.exports= (function(){
-    var getusers= function(req, res){
+    var getuser= function(req, res){
         res.send("lista utenti");
     };
+    var createuser = function(req, res){
+        res.send("creazione utente");
+    };
+    var detailuser=function(req, res){
+        var id= req.params.id;
+        res.send("dettaglio utente con id:"+id);
+    };
+    var deleteuser=function(req, res){
+        var id= req.params.id;
+        res.send("eliminato utente con id:" +id)
+    };
+    var updateuser=function(req, res){
+        var id= req.params.id;
+        res.send("aggiorno utente con id:" +id)
+    };
+
     return{
-        getusers: getusers
-    }
+        getuser: getuser,
+        createuser :createuser,
+        detailuser: detailuser,
+        deleteuser:deleteuser,
+        updateuser:updateuser
+    };
 })();
