4c1b33b851e2627eb27a5f3b829ffc4c40a56241
leo <njuccon@hotmail.it>
2017-03-08 15:54:16 +0100

    aggiunto indexhtml, jquery e bootstrap

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express= require('express');
+var path = require ('path');
+var app = express();
+const PORT = 3000;
+
+
+//BOOTSTRAP
+
+app.use('/bootstrap', express.static(path.join(__dirname, "..", "node_modules","bootstrap", "dist")));
+
+//JQUERY
+app.use('/jquery', express.static(path.join(__dirname, "..", "node_modules","jquery", "dist")));
+
+
+
+//SERVIAMO LA INDEX
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname,"..", "client", "index.html"));
+})
+
+
+
+
+//START SERVER
+app.listen(PORT, function(err){
+        console.log("server connesso su http://localhost:" +PORT);
+                                });
